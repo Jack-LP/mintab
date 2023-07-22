06ccce07bee3eeaee34621e44fc8e5a9de41cafd
Jack-LP <jackpaget1@gmail.com>
2023-07-22 19:54:40 +0100

    Added favicon and console message

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'preact/hooks';
+import { useState, useEffect } from 'preact/hooks';
 import { Wallpaper } from './components/display/Wallpaper';
 import { SettingsBtn } from './components/display/SettingsBtn';
 import { SettingsDrawer } from './components/settings/SettingsDrawer';
@@ -7,9 +7,17 @@ import { Clock } from './components/display/Clock';
 import { Greeting } from './components/display/Greeting';
 import { Search } from './components/display/Search';
 import { Links } from './components/display/Links';
+import { getFromStorage, setToStorage } from './utilities/localStorage';
+import { printConsole } from './utilities/printConsole';
 
 export const App = () => {
   const [showDrawer, setShowDrawer] = useState(false);
+  const [msgLogged, setMsgLogged] = useState(false);
+
+  if (!msgLogged) {
+    printConsole();
+    setMsgLogged(true);
+  }
 
   return (
     <div className='relative flex min-h-screen w-screen items-center justify-center overflow-hidden font-nunito text-white'>
